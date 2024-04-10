2b2274708462bf63142fd0d2a38029a21d107af4
joachim69 <60317486+JoachimBose@users.noreply.github.com>
2024-04-10 16:39:50 +0200

    got VR hmd and movement to work

--- Assets/Scripts/Player/PlayerInputSystem.cs
+++ Assets/Scripts/Player/PlayerInputSystem.cs
@@ -76,19 +76,29 @@ namespace Opencraft.Player
                     input.ValueRW.SecondaryAction.Set();
                     playerController.inputSecondaryAction= false;
                 }
-                
-                // Look
-                input.ValueRW.Pitch = math.clamp(input.ValueRW.Pitch + playerController.inputLook.y, -math.PI / 2,
-                    math.PI / 2);
-                input.ValueRW.Yaw = math.fmod(input.ValueRW.Yaw + playerController.inputLook.x, 2 * math.PI);
-            
 
                 // Sync camera to look
-                playerObj.transform.rotation = math.mul(quaternion.RotateY(input.ValueRO.Yaw),
-                    quaternion.RotateX(-input.ValueRO.Pitch));
-                //var offset = math.rotate(playerObj.transform.rotation, new float3(0)/*_cameraOffset*/);
-                playerObj.transform.position = localToWorld.ValueRO.Position + _cameraOffset;
-                
+                if (playerController.isVrPlayer())
+                {
+                    // MultiplayPlayerControllerVR playerControllerVR = ((MultiplayPlayerControllerVR)playerController);
+                    // input.ValueRW.Pitch = playerControllerVR.inputLookVR.eulerAngles.y;
+                    // input.ValueRW.Yaw = playerControllerVR.inputLookVR.eulerAngles.z;
+                    // playerObj.transform.rotation = playerControllerVR.inputLookVR;
+                    playerObj.transform.position = localToWorld.ValueRO.Position + _cameraOffset;
+                }
+                else {
+                    // Look
+                    input.ValueRW.Pitch = math.clamp(input.ValueRW.Pitch + playerController.inputLook.y, -math.PI / 2,
+                        math.PI / 2);
+                    input.ValueRW.Yaw = math.fmod(input.ValueRW.Yaw + playerController.inputLook.x, 2 * math.PI);
+
+
+                    
+                    playerObj.transform.rotation = math.mul(quaternion.RotateY(input.ValueRO.Yaw),
+                        quaternion.RotateX(-input.ValueRO.Pitch));
+                    //var offset = math.rotate(playerObj.transform.rotation, new float3(0)/*_cameraOffset*/);
+                    playerObj.transform.position = localToWorld.ValueRO.Position + _cameraOffset;
+                }
             }
         }
     }
